da1edc73083086625e33bb235634c7f67c7a1ab8
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2026-03-21 17:35:40 +0100

    Refactor: use init for util argument parsing

--- src/main.zig
+++ src/main.zig
@@ -1,35 +1,54 @@
 const std = @import("std");
 const parser = @import("parser.zig");
 const termutils = @import("termutils.zig");
-const program = @import("program.zig");
 const utils = @import("utils.zig");
 
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    const stdin = std.io.getStdIn().reader();
+const Program = @import("program.zig").Program;
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer {
-        const deinit_status = gpa.deinit();
-        if (deinit_status == .leak) @panic("Memory Leak");
-    }
-    const allocator = gpa.allocator();
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    const gpa = init.gpa;
+
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writerStreaming(io, &stdout_buf);
+    const stdout = &stdout_writer.interface;
+
+    var stderr_buf: [1024]u8 = undefined;
+    var stderr_writer = std.Io.File.stderr().writerStreaming(io, &stderr_buf);
+    const stderr = &stderr_writer.interface;
+
+    var stdin_buf: [1024]u8 = undefined;
+    var stdin_reader = std.Io.File.stdin().readerStreaming(io, &stdin_buf);
+    const stdin = &stdin_reader.interface;
 
     // Parse command line arguments
-    const path = utils.getPathArg(allocator) catch {
-        return;
-    };
-    defer allocator.free(path);
+    const path = utils.extractPathArg(gpa, init.minimal.args) catch |err|
+        switch (err) {
+            error.MissingPathArgument => {
+                try stderr.writeAll("No path provided");
+                try stderr.flush();
+                std.process.exit(1);
+            },
+            else => return err,
+        };
+    defer gpa.free(path);
 
-    var p = try program.Program.init(
-        allocator,
-        stdout.any(),
-        stdin.any(),
+    if (!utils.validatePath(io, path)) {
+        try stderr.print("Path is invalid: {s}", .{path});
+        try stderr.flush();
+        std.process.exit(1);
+    }
+
+    var program = try Program.init(
+        init.gpa,
+        stdout,
+        stdin,
         path,
     );
-    defer p.deinit();
-    p.setup();
-    try p.run();
+    defer program.deinit();
+
+    program.setup();
+    try program.run();
 }
 
 test {
